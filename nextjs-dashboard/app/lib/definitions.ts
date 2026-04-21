178d195abbc7247b357bf1180cb854500536ebb7
louisatyas889 <louisatyas832@gmail.com>
2026-04-22 00:23:36 +0700

    Beneran fix export Revenue dan Customers

--- nextjs-dashboard/app/lib/definitions.ts
+++ nextjs-dashboard/app/lib/definitions.ts
@@ -1,30 +1,3 @@
-// app/lib/definitions.ts
-
-export interface Vessel {
-  id: string;
-  dest: string;
-  status: 'EN ROUTE' | 'IN PORT' | 'DELAYED' | 'MAINTENANCE' | 'STORM' | 'IN TRANSIT' | 'APPROACHING' | 'LOADING' | 'DEPARTING' | 'DEPARTURE PHASE';
-  statusColor: string;
-  eta: string;
-  etaColor: string;
-  mon: string;
-}
-
-export interface Alert {
-  id?: number; // Pakai tanda tanya karena di DB SERIAL (otomatis), tapi di dummy mungkin belum ada
-  type: string;
-  tc: string;
-  time: string;
-  body: string;
-}
-
-export interface Fuel {
-  c: string;
-  h: number;
-  l: string;
-}
-
-// Pastikan ada kata 'export' di depannya
 export type Revenue = {
   month: string;
   revenue: number;
@@ -38,9 +11,5 @@ export interface CustomersTableType {
   total_invoices: number;
   total_pending: number;
   total_paid: number;
-};
-
-export interface FormattedCustomersTable extends CustomersTableType {
-  // Jika ada tambahan format khusus, tambahkan di sini, 
-  // tapi biasanya ini sudah cukup untuk table.tsx
 }
+export interface FormattedCustomersTable extends CustomersTableType {}
