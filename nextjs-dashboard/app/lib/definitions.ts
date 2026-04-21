fd9bb515f0b09edb201bbb7223730cbd84fd21e3
louisatyas889 <louisatyas832@gmail.com>
2026-04-21 23:43:16 +0700

    perubahan

--- nextjs-dashboard/app/lib/definitions.ts
+++ nextjs-dashboard/app/lib/definitions.ts
@@ -29,3 +29,18 @@ export type Revenue = {
   month: string;
   revenue: number;
 };
+
+export interface CustomersTableType {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  total_invoices: number;
+  total_pending: number;
+  total_paid: number;
+};
+
+export interface FormattedCustomersTable extends CustomersTableType {
+  // Jika ada tambahan format khusus, tambahkan di sini, 
+  // tapi biasanya ini sudah cukup untuk table.tsx
+}
